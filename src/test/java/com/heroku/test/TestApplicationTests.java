af612c1d9c0b772f9d90cc507bc6c6219b0f2d2c
kwamekert <tomriddle477@gmail.com>
2020-03-24 13:52:05 +0000

    First commit

---
+++ src/test/java/com/heroku/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroku.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
